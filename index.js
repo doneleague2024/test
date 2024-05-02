8253f728ac1c26817f4b9f20ccebe8b1f9579606
shimizuryosuke1026 <168202888+shimizuryosuke1026@users.noreply.github.com>
2024-05-02 17:33:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ $(function(){
     $('.slider').slick();
     //$(クラス名).slick();で使用できます。
 })
+$(function(){
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:2000,
+        fade:true,
+        arrows: false,
+    });
+})
