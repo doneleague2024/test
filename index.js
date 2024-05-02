3c05b2bf549b1ef293e5e6710b865a8b55c46000
shimizuryosuke1026 <168202888+shimizuryosuke1026@users.noreply.github.com>
2024-05-02 17:40:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 
 $(function(){
-    $('.slider').slick({
-        autoplay:true,
-        autoplaySpeed:2000,
-        fade:true,
-        arrows: true,
-        dots: true, // インジケーター
-    });
     $(".slide-items").slick({
         autoplay: true, // 自動再生
         arrows: true, // 矢印
